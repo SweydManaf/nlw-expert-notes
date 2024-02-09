2c19452802b5649b4e0ff39a3af4e56cb2203491
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2024-02-10 01:31:04 +0200

    feat: add note

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
 import logo from "./assets/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
 export function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      date: new Date(),
+      content: "Hello World",
+    },
+  ]);
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: Math.random(),
+      date: new Date(),
+      content,
+    };
+
+    setNotes([newNote, ...notes]);
+  }
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW Expert" />
@@ -18,9 +36,10 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <NewNoteCard />
-        <NoteCard note={{ date: new Date(), content: "hello world" }} />
-        <NoteCard note={{ date: new Date(), content: "hello world" }} />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+        {notes.map((note) => {
+          return <NoteCard note={note} key={note.id} />;
+        })}
       </div>
     </div>
   );
